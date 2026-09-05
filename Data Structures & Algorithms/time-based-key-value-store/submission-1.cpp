31a8e86e9e6aa385108ecf58cfc8447c0d4c0242
Yash Vardhan Singh <yashvardhansingh7465@gmail.com>
2026-09-05 11:25:58 +0530

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.cpp
@@ -0,0 +1,27 @@
+class TimeMap {
+public:
+    unordered_map<string, vector<pair<int, string>>> tmp;
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        tmp[key].emplace_back(timestamp, value);
+    }
+    
+    string get(string key, int timestamp) {
+        const auto& searchList = tmp[key];
+        int l=0,r=searchList.size()-1;
+        string res="";
+        while(l<=r) {
+            int m=l+(r-l)/2;
+            if(searchList[m].first<=timestamp) {
+                l=m+1;
+                res=searchList[m].second;
+                }
+            else r=m-1;
+        }
+        return res;
+
+    }
+};
